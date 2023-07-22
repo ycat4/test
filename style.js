af694aa9a97dfb7f121c5aeabeea099ab4ecaeff
ycat4 <140232253+ycat4@users.noreply.github.com>
2023-07-22 15:31:07 +0900

    Create style.js

---
+++ style.js
@@ -0,0 +1,20 @@
+<script>
+(window.onload = function() {
+
+  // フェードイン処理
+  jQuery(window).scroll(function (){
+    jQuery(".pade").each(function(){
+      var winheight = jQuery(window).height();
+      var posi = jQuery(this).offset().top;
+      var scroll = jQuery(window).scrollTop();
+      if (scroll + winheight > posi){
+        jQuery(this).addClass("padein");
+      } else {
+        //　スクロールで画面上部に戻った際に要素を非表示にしたい場合は、下記の行のコメントを外し有効にしてください。
+        jQuery(this).removeClass("padein");
+      }
+    });
+   });
+
+})(); 
+</script>
